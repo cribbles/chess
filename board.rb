8b3e9f7db7edfe8cc471b6b4fedfddc85ad31d58
cribbles <yaphetf@gmail.com>
2015-08-04 12:09:08 -0400

    Add Board#on_board? and bracket methods

--- board.rb
+++ board.rb
@@ -4,4 +4,21 @@ class Board
   def initialize(size = BOARD_SIZE)
     @grid = Array.new(size) { Array.new(size) }
   end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos)
+    x, y = pos
+    grid[x][y] = pos
+  end
+
+  def on_board?(pos)
+    pos.all? { |coord| coord.between?(0, BOARD_SIZE-1) }
+  end
+
+  private
+  attr_reader :grid
 end
